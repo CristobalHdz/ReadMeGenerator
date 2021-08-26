04110e55916b2c2371308dcc414027cf8b1f8f90
CristobalHdz <cristobalhdzch@gmail.com>
2021-08-25 21:55:51 -0600

    Add README format

--- index.js
+++ index.js
@@ -1,14 +1,111 @@
 // TODO: Include packages needed for this application
-const fs = require('fs')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// "https://javascript.plainenglish.io/how-to-inquirer-js-c10a4e05ef1f" Inquirer usage
+// https://gist.github.com/Amitabh-K/ae073eea3d5207efaddffde19b1618e8 Inquirer check email
+// https://shields.io/ Shield creator
+// https://opensource.org Licenses
+
+// https://github.com/hoffman1200/Homework-9/tree/master/Develop
+// https://www.youtube.com/watch?v=tshItrUIPYw&ab_channel=KazzuallGamer
+
+const questions = [
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter your project's description",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Enter your email",
+        name: "email",
+        validate: function (email) {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+            if (valid) {
+                console.log("email has been stored")
+                return true
+            } else {
+                console.log(". Please enter a valid email")
+                return false
+            }
+        }
+    },
+    {
+        type: "confirm",
+        message: "Are there any contributors?",
+        name: "contributorConfirm",
+    },
+    {
+        type: "input",
+        message: "Enter contributors",
+        name: "contributors",
+        when: function (inputConfirm) {
+            return inputConfirm.contributorConfirm
+        }
+    },
+    {
+        type: "confirm",
+        message: "Do you want to give installation notes?",
+        name: "instConfirm"
+    },
+    {
+        type: "Input",
+        message: "Please enter installation notes",
+        name: "installation",
+        when: function (inputConfirm) {
+            return inputConfirm.instConfirm
+        }
+    },
+    {
+        type: "confirm",
+        message: "Do you want to add instructions for your project's usage?",
+        name: "instructionsConfirm"
+    },
+    {
+        type: "Input",
+        message: "Please enter the project instructions",
+        name: "instructions",
+        when: function (instructionsCon) {
+            return instructionsCon.instructionsConfirm
+        }
+    },
+    {
+        type: "list",
+        message: "Please select a license",
+        name: "license",
+        choice: ["Mozilla", "Apache", "MIT", "Apache License 2.0", "Common Developer 1.0", "No license"]
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log("Your README file has been proppertly created")
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inputs) => {
+        const data = generateMarkdown(inputs);
+        console.log(inputs)
+
+        writeToFile("README.md", inputs)
+    });
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,51 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Mozilla") {
+    badge = `[![License](https://img.shields.io/badge/License-Mozilla-green)](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license === "Apache") {
+    badge = `[![License](https://img.shields.io/badge/License-Apache-blue)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === "MIT") {
+    badge = `[![License](https://img.shields.io/badge/License-MIT-green)](https://opensource.org/licenses/MIT)`
+  } else if (license === "OVL GPL") {
+    badge = `[![License](https://img.shields.io/badge/License-Apache-blue)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === "Common Developer 1.0") {
+    badge = `[![License](https://img.shields.io/badge/License-CDDL-green)](https://opensource.org/licenses/CDDL-1.0)`
+  } else {
+    badge = `[![License](https://img.shields.io/badge/License-No_License-red)]`
+  }
+  return badge
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ${renderLicenseBadge}
+
+  ## Table of Contents
+  * [Project owner info](#username)
+  * [Project Description](#description)
+  * [Project contributors](#contributors)
+  * [Instalation Notes](#installation)
+  * [Project Instructions](#instructions)
+
+  ## Project owner info
+  Github info
+  [${data.username}] github.com/${data.username}
+  [${data.email}]
+
+  ## Project Description
+  ${data.description}
+
+  ## Project contributors
+  ${data.contributors}
+
+  ## Instalation Notes
+  ${data.installation}
+
+  ## Project Instructions
+  ${data.instructions}
 
 `;
 }
